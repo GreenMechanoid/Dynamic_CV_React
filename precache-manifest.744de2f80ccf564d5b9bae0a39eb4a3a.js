ecfb8c7e7432161f92884a6ccbb0995da372acf6
Daniel Svensson <daniel.svensson682@gmail.com>
2023-02-24 15:41:58 +0100

    Deploy React app to GitHub Pages

---
+++ precache-manifest.744de2f80ccf564d5b9bae0a39eb4a3a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b3fcd1b2f25fe352184",
+    "url": "/static/css/main.1cba0f97.chunk.css"
+  },
+  {
+    "revision": "9b3fcd1b2f25fe352184",
+    "url": "/static/js/main.9b3fcd1b.chunk.js"
+  },
+  {
+    "revision": "32ab41aa145553738579",
+    "url": "/static/js/1.32ab41aa.chunk.js"
+  },
+  {
+    "revision": "bbc0d0176a3e89b6816a",
+    "url": "/static/css/2.d08efbfb.chunk.css"
+  },
+  {
+    "revision": "bbc0d0176a3e89b6816a",
+    "url": "/static/js/2.bbc0d017.chunk.js"
+  },
+  {
+    "revision": "6b461d50a6d7cb126876",
+    "url": "/static/js/runtime~main.6b461d50.js"
+  },
+  {
+    "revision": "58f5b48eb22c9a6871ee87f18a87699a",
+    "url": "/static/media/cv_pic2.58f5b48e.jpg"
+  },
+  {
+    "revision": "71919178fde916752e99e108da4c1e11",
+    "url": "/static/media/Cutie_Fluttershy.71919178.jpg"
+  },
+  {
+    "revision": "7688d28445f38608fa097cc41b060893",
+    "url": "/static/media/grumpycat.7688d284.jpg"
+  },
+  {
+    "revision": "3f33888ecdc2346d10da0e3feac5de95",
+    "url": "/index.html"
+  }
+];
