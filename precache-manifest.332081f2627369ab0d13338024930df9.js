cdcb2fefc3cad2f43714520c2e6917656bf69a8f
Daniel Svensson <daniel.svensson682@gmail.com>
2023-02-24 15:49:14 +0100

    Deploy React app to GitHub Pages

---
+++ precache-manifest.332081f2627369ab0d13338024930df9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b3fcd1b2f25fe352184",
+    "url": "/Dynamic_CV_React/static/css/main.1cba0f97.chunk.css"
+  },
+  {
+    "revision": "9b3fcd1b2f25fe352184",
+    "url": "/Dynamic_CV_React/static/js/main.9b3fcd1b.chunk.js"
+  },
+  {
+    "revision": "32ab41aa145553738579",
+    "url": "/Dynamic_CV_React/static/js/1.32ab41aa.chunk.js"
+  },
+  {
+    "revision": "bbc0d0176a3e89b6816a",
+    "url": "/Dynamic_CV_React/static/css/2.d08efbfb.chunk.css"
+  },
+  {
+    "revision": "bbc0d0176a3e89b6816a",
+    "url": "/Dynamic_CV_React/static/js/2.bbc0d017.chunk.js"
+  },
+  {
+    "revision": "29789d79307fca0ad221",
+    "url": "/Dynamic_CV_React/static/js/runtime~main.29789d79.js"
+  },
+  {
+    "revision": "58f5b48eb22c9a6871ee87f18a87699a",
+    "url": "/Dynamic_CV_React/static/media/cv_pic2.58f5b48e.jpg"
+  },
+  {
+    "revision": "71919178fde916752e99e108da4c1e11",
+    "url": "/Dynamic_CV_React/static/media/Cutie_Fluttershy.71919178.jpg"
+  },
+  {
+    "revision": "7688d28445f38608fa097cc41b060893",
+    "url": "/Dynamic_CV_React/static/media/grumpycat.7688d284.jpg"
+  },
+  {
+    "revision": "28f415ac26640ba3c563e047ac26a611",
+    "url": "/Dynamic_CV_React/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.744de2f80ccf564d5b9bae0a39eb4a3a.js"
+  "/Dynamic_CV_React/precache-manifest.332081f2627369ab0d13338024930df9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Dynamic_CV_React/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
