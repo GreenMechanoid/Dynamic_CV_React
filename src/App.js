6ed8f58c1c73883a5126f0b0f97f93bd185abee1
Daniel Svensson <daniel.svensson682@gmail.com>
2023-02-24 15:26:05 +0100

    updated readme.md

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import { NavbarComponent } from "./components/NavBar.js";
+import { HomeComponent } from "./components/Home.js";
+import { CvComponent } from "./components/Cv.js";
+import { Portfolio } from "./components/Portfolio.js";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import React, { useState , useEffect } from "react";
+import ModalSecret from "./components/Secrets";
 
-function App() {
+export default function App() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const myKey = "fluffy"
+  let keyList = ""  
+  useEffect(()=> {
+    document.addEventListener('keydown', detectedKey,true)
+  },)
+
+  const detectedKey = (e) => {
+    keyList += e.key;
+    console.log("clicked: ", e.key)
+    if (myKey === keyList) {
+      setModalOpen(true);
+    }
+    else{
+      setTimeout(()=>{
+        keyList = "";
+      },2000)
+
+      
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <NavbarComponent/>
+     <div className="App">
+      {modalOpen && <ModalSecret setOpenModal={setModalOpen} />}
     </div>
-  );
-}
+      <Routes>
+        <Route path="/" element={<HomeComponent/>} />
+        <Route path="/cv" element={<CvComponent/>} />
+        <Route path="/Portfolio" element={<Portfolio/>} />
+      </Routes>
+  </Router>
 
-export default App;
+  )
+}
